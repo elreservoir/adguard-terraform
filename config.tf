a1d597b55a5592c55c2cc5991611793161d85e0d
Lucas Weihermann <LuckyW775@gmail.com>
2025-04-28 16:20:29 +0200

    add dns config

--- config.tf
+++ config.tf
@@ -1,6 +1,14 @@
 resource "adguard_config" "adguard_config" {
   stats = {
-    enabled  = false
+    enabled  = true
     interval = 2160
+  }
+
+  dns = {
+    upstream_dns = ["https://dns.quad9.net/dns-query", "https://dns.google/dns-query", "https://dns.cloudflare.com/dns-query"]
+    rate_limit = 0
+    dnssec_enabled = true
+    use_private_ptr_resolvers = true
+    local_ptr_upstreams = [ "192.168.178.1" ]
   }
 }
