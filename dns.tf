4dda20b3ef22c0bfab0ce9dcaa8882f6763f5a11
Administrator <elreservoir+root@gmail.com>
2025-05-01 08:56:16 +0000

    add pm dns rewrite

--- dns.tf
+++ dns.tf
@@ -2,3 +2,8 @@ resource "adguard_rewrite" "elreservoir_home" {
   domain = "*.elreservoir.home"
   answer = "192.168.178.19"
 }
+
+resource "adguard_rewrite" "proxmox_lan" {
+  domain = "pm.elreservoir.lan"
+  answer = "192.168.178.135"
+}
